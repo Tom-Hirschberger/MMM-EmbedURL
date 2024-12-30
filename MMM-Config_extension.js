b40128217779a6b6d46172e8fcfad9766fe464cc
sam detweiler <sdetweil@gmail.com>
2024-12-30 16:08:06 -0600

    switch to css for color

--- MMM-Config_extension.js
+++ MMM-Config_extension.js
@@ -1,5 +1,5 @@
 // on form_loaded event
-$(document).on('form_loaded', function () {
+$(document).on('form_loaded1', function () {
 	// find all the elements of our when selection list and get the selected option in each
 	let counter = 0
 	$('.m_MMM-EmbedURL .embed').each(
